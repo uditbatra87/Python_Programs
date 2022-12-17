87f1a1a9432c83f9b844fbe0bebc4bb666938016
uditbatra <uditbatra87@gmail.com>
2022-12-17 12:44:48 +0530

    modified Hello.py

--- 01_Print_List.py
+++ 01_Print_List.py
@@ -2,3 +2,14 @@ my_list = [1,2,3,4,5]
 
 for i in my_list:
     print(i)
+    
+
+# Init Dictionary
+my_dict = {"name" : "udit","age" : "35", "location" : "pune"}
+
+for key,value in my_dict.items():
+    print(key + " : " + value)
+
+
+
+
